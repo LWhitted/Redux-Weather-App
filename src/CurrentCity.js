d013cb35fe7a211ea8a269e522cc2ab91b0ce0ef
Lauren Whitted <lauren.whitted13@gmail.com>
2023-02-28 19:08:59 -0600

    set up REACT/REDUX and added component files with description

---
+++ src/CurrentCity.js
@@ -0,0 +1,4 @@
+// This will be the page that you get redirected to when you hit the search button from
+// the home page.
+// the data on this page should be City Name, current temp, feels like, weather description, Air quality, link to air quality (more Info)
+
---
+++ src/Home.js
@@ -0,0 +1,5 @@
+// this will be the landing page for the site
+// it should be a simple landing page that welcomes you and has the search bar so you
+// can search your city
+// the search button should search the API for the city and simultaneously take you to the currentcity page which populates the data
+// wanted from the openweather API and the airquality api
