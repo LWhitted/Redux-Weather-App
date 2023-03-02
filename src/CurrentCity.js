a5c1d1476a1bce1a5e7897af34d91daa2e28e214
Lauren Whitted <lauren.whitted13@gmail.com>
2023-03-02 17:34:37 -0600

    getting the home page to work properly

--- src/CurrentCity.js
+++ src/CurrentCity.js
@@ -1,4 +1,8 @@
 // This will be the page that you get redirected to when you hit the search button from
 // the home page.
 // the data on this page should be City Name, current temp, feels like, weather description, Air quality, link to air quality (more Info)
+import React from 'react';
 
+const currentCity = () => {
+    return 
+}
