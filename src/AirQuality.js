954591f11182833c29e5ba3557461effa3f76bf4
Lauren Whitted <lauren.whitted13@gmail.com>
2023-03-07 15:10:14 -0600

    updating the css for components

--- src/AirQuality.js
+++ src/AirQuality.js
@@ -3,6 +3,8 @@ import { useCallback, useEffect, useState } from 'react';
 import { selectCityName } from './State/City/Selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import './AirQuality.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const AirQuality = () => {
     const [data, setAirQualityData] = useState();
@@ -20,14 +22,18 @@ const AirQuality = () => {
         fetchAQData();
     }, [fetchAQData]);
     return <div className="airQuality">
-        {/* <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Air Quality</h5> */}
-                <p>Your current air quality is: {data?.status}.</p>
-                <p>Want more info on your city's air quality? <a href={data?.data.city.url}>CLICK HERE</a></p>
-            </div>
-    //     </div>
-    // </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/AQIndex.jpeg" />
+            <Card.Body>
+                <Card.Title>Air Quality</Card.Title>
+                <Card.Text>
+                    <p>Your current air quality is: {data?.status}.</p>
+                    <p>Want more info on your city's air quality?</p>
+                </Card.Text>
+                <Button variant="primary" href="{data?.data.city.url}">Click Here</Button>
+            </Card.Body>
+        </Card>
+    </div>
 }
 export default AirQuality;
 
--- src/CurrentCity.js
+++ src/CurrentCity.js
@@ -3,12 +3,13 @@
 // the data on this page should be City Name, current temp, feels like, weather description, Air quality, link to air quality (more Info)
 import React from 'react';
 import { useCallback, useEffect, useState } from 'react';
-import {selectCityName} from './State/City/Selectors';
-import {useDispatch, useSelector} from 'react-redux';
+import { selectCityName } from './State/City/Selectors';
+import { useDispatch, useSelector } from 'react-redux';
 import './CurrentCity.css';
+import Card from 'react-bootstrap/Card';
 
 const CurrentCity = () => {
-    const cityName = useSelector((state)=>selectCityName(state))
+    const cityName = useSelector((state) => selectCityName(state))
     // const fetchCityData = React.useCallback(async (props) => {
     //     const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=398d9f85036ab66f822fd82148535c81&units=imperial`)
 
@@ -17,35 +18,38 @@ const CurrentCity = () => {
     //     const feelsLike = weatherData.main.feels_like;
     //     const weatherDescription = weatherData.weather[0].description;
     // })
-   const [weatherData, setWeatherData]= useState();
+    const [weatherData, setWeatherData] = useState();
 
     const fetchWeatherData = () => {
         return fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=398d9f85036ab66f822fd82148535c81&units=imperial`
         ).then((response) => response.json());
-      };
-      
-      const fetchData = useCallback(async () => {
+    };
+
+    const fetchData = useCallback(async () => {
         const a = await fetchWeatherData();
         setWeatherData(a)
-      }, []);
-      
-      useEffect(() => {
+    }, []);
+
+    useEffect(() => {
         fetchData();
-      }, [fetchData]);
+    }, [fetchData]);
 
     return <div className="currentCity">
-        {/* <div class="card">
-            <div class="card-body"> */}
-        <h1>{weatherData?.name}</h1>
-        <h2>Current Temp is {weatherData?.main.temp}</h2>
-        <h2>Feels Like {weatherData?.main.feels_like}</h2>
-        <h2>The current weather conditions are {weatherData?.weather[0].description}</h2>
-        </div>
-    //     </div>
-    // </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/weatherIcons.jpeg" />
+            <Card.Body>
+                <Card.Title>{weatherData?.name}</Card.Title>
+                <Card.Text>
+                    <h2>Current Temp is {weatherData?.main.temp}</h2>
+                    <h2>Feels Like {weatherData?.main.feels_like}</h2>
+                    <h2>The current weather conditions are {weatherData?.weather[0].description}</h2>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </div>
 
-   
+
 }
 
 export default CurrentCity;
